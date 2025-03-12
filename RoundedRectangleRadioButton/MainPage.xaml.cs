d09cca4f05a0b08c5165051b44d102ae01fb2148
AravinthSundaramSF <aravinth.sundaram@syncfusion.com>
2025-03-12 18:27:08 +0530

    Create Rounded Rectangle Radio Buttons and Maintain Group Selection in .NET MAUI

---
+++ RoundedRectangleRadioButton/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace RoundedRectangleRadioButton
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+
+}
